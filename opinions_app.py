bb6bf9c92be75b60902caa8d933315bee25183d2
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2025-04-21 22:47:52 +0500

    Переиспользование и наследование шаблонов

--- opinions_app.py
+++ opinions_app.py
@@ -42,12 +42,12 @@ def index_view():
     # ...и определяется случайный объект:
     opinion = Opinion.query.offset(offset_value).first()
     # Передаём в шаблон весь объект opinion:
-    return render_template('index.html', opinion=opinion)
+    return render_template('opinion.html', opinion=opinion)
 
 
 @app.route('/add')
 def add_opinion_view():
-    return 'Страница в разработке'
+    return render_template('add_opinion.html')
 
 
 if __name__ == '__main__':
