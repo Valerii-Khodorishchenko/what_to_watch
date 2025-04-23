c96c008973fa897bb09b055eea0cf9a6ef8fd9cd
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2025-04-23 16:44:03 +0500

    Пользовательские команды Flask

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,9 @@
 import os
+import csv
 from datetime import datetime
 from random import randrange
 
+import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -142,6 +144,27 @@ def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     # ... и передать его в шаблон (тот же что и для главной страницы):
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открываем файл:
+    with open('opinions.csv', encoding='utf-8') as f:
+        # Создаём итерируемый объект, который отображает каждую строку
+        # в качестве словаря с ключами из шапки файла:
+        reader = csv.DictReader(f)
+        # Для подсчёта строк добавляем счётчик:
+        counter = 0
+        for row in reader:
+            # Распакованный словарь используем
+            # для создания экземпляра модели Opinion:
+            opinion = Opinion(**row)
+            # Добавляем объект в сессию и коммитим:
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнение: {counter}')
 
 
 if __name__ == '__main__':
