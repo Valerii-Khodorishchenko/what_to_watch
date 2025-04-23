a45626fc09b4db70a9797a19158da933405c7724
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2025-04-24 00:54:30 +0500

    API без сторонних библиотек. Получение отдельного объекта

---
+++ opinions_app/api_views.py
@@ -0,0 +1,25 @@
+from flask import jsonify
+
+from . import app
+from .models import Opinion
+
+
+def opinion_to_dict(opinion):
+    return dict(
+        id = opinion.id,
+        title = opinion.title,
+        text = opinion.text,
+        source = opinion.source,
+        timestamp = opinion.timestamp,
+        added_by = opinion.added_by
+    )
+
+
+# Явно разрешаем метод GET:
+@app.route('/api/opinions/<int:id>/', methods=['GET'])
+def get_opinion(id):
+    # Получаем объект по id или выбрасываем ошибку 404:
+    opinion = Opinion.query.get_or_404(id)
+    # Конвертируем объект модели в словарь:
+    data = opinion_to_dict(opinion)
+    return jsonify({'opinion': data}), 200
