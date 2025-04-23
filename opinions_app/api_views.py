f535596247b0a8b44fc783d2e0ac7aff6df467fd
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2025-04-24 01:34:21 +0500

    Обновление отдельного объекта: Update

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,6 +1,6 @@
-from flask import jsonify
+from flask import jsonify, request
 
-from . import app
+from . import app, db
 from .models import Opinion
 
 
@@ -10,4 +10,22 @@ def get_opinion(id):
     # Получаем объект по id или выбрасываем ошибку 404:
     opinion = Opinion.query.get_or_404(id)
     # Никаких лишних функций, просто метод to_dict():
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=['PATCH'])
+def update_opinion(id):
+    data = request.get_json()
+    # Если метод get_or_404 не найдёт указанный ID,
+    # то он выбросит исключение 404:
+    opinion = Opinion.query.get_or_404(id)
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
+    # Все изменения нужно сохранить в базе данных.
+    # Объект opinion добавлять в сессию не нужно.
+    # Этот объект получен из БД методом get_or_404() и уже храниться в сессии.
+    db.session.commit()
+    # При изменении объекта вернёт сам объект и код 200:
     return jsonify({'opinion': opinion.to_dict()}), 200
